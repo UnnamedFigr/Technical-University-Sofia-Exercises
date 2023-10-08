064fd4c45ced41f42d7342b72897e2e5087e74f9
Ivan Drazhev <ivan@Ivans-MacBook-Air.local>
2023-10-08 15:37:31 +0300

    Added exercises

--- Programing_Basics_C_Sharp/ExercisesLoops/ExercisesLoops/Program.cs
+++ Programing_Basics_C_Sharp/ExercisesLoops/ExercisesLoops/Program.cs
@@ -4,7 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Въвеждане на цяла стойност от потребител, организирана в цикъл,
+        // който се изпълнява докато потребителя не въведе валидна целочислина стойност
+        int n;
+        bool ok;
+        do
+        {
+            Console.WriteLine("Моля, въведете цяло число");
+
+            ok = int.TryParse(Console.ReadLine(), out n);
+            if (ok)
+                Console.WriteLine(n);
+            else
+                Console.WriteLine("Не сте въвел цяло число");
+        } while (!ok);
     }
 }
 
