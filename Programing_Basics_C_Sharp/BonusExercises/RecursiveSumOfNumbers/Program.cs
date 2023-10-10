1abb6c96ad8808587724e0e935cf96008077f1b5
Ivan Drazhev <ivan@Ivans-MacBook-Air.local>
2023-10-10 17:31:45 +0300

    Initial commit

---
+++ Programing_Basics_C_Sharp/BonusExercises/RecursiveSumOfNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace RecursiveSumOfNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Напишете рекурсивна функция,
+        //която намира сумата на първите n
+        //естествени числа, където n се определя от потребителя
+        Console.WriteLine("Въведете до кое число искате да се намери сумата:");
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0; 
+        RecursiveSum(n, ref sum);
+        Console.WriteLine($"The sum is: {sum}");
+
+    }
+
+    static void RecursiveSum(int n, ref int sum)
+    {
+
+        if(n > 0)
+        {
+            sum += n;
+            RecursiveSum(n - 1, ref sum);
+        }
+    }
+}
+
