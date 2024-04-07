f015582a3f01369a4ca07b85d43e55f976fdfeff
Ivan Drazhev <justdrazhev1@gmail.com>
2024-04-07 21:19:00 +0300

    test commit

---
+++ Second_year/Coursework_HtmlCrawler/HtmlCrawler/HtmlCrawlerIndependentTest/ElementFlag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlCrawlerIndependentTest
+{
+    internal enum ElementFlag
+    {
+        CData = 1,
+
+        Empty = 2,
+
+        Closed = 4,
+
+        CanOverlap = 8
+    }
+}
--- Second_year/Coursework_HtmlCrawler/HtmlCrawler/HtmlCrawlerIndependentTest/Program.cs
+++ Second_year/Coursework_HtmlCrawler/HtmlCrawler/HtmlCrawlerIndependentTest/Program.cs
@@ -4,18 +4,27 @@
     {
         static void Main(string[] args)
         {
-            string path = "C:\\Users\\kasap\\Documents\\Repositories\\CourseWork_HtmlCrawler_v1.2\\HtmlCrawler\\HtmlCrawlerIndependentTest\\testweb.html";
-            string content = "";
-            if (!File.Exists(path))
-            {
-                throw new FileNotFoundException("File not found");
-            }
-            content = File.ReadAllText(path);
-            DocParser parser = new DocParser();
-            parser.ParseHTML(content);
-            Node htmlNode = parser.GetRootNode();
-            QueryHandler handler = new QueryHandler(htmlNode);
-            handler.HandleQuery("PRINT //html");
+            //string path = "C:\\Users\\kasap\\Documents\\Repositories\\CourseWork_HtmlCrawler_v1.2\\HtmlCrawler\\HtmlCrawlerIndependentTest\\testweb.html";
+            //string content = "";
+            //if (!File.Exists(path))
+            //{
+            //    throw new FileNotFoundException("File not found");
+            //}
+            //content = File.ReadAllText(path);
+            //DocParser parser = new DocParser();
+            //parser.ParseHTML(content);
+            //Node htmlNode = parser.GetRootNode();
+            //QueryHandler handler = new QueryHandler(htmlNode);
+            //handler.HandleQuery("PRINT //html");
+
+            HtmlDocument document = new HtmlDocument();
+            document.Text = @"<p>This is a paragraph tag.</p>";
+            HtmlNode s = new HtmlNode(HtmlNodeType.Text, document, 2);
+            string text = "This is a test text";
+            s.OwnerDocument = document;
+            s.SetName("p");
+            Console.WriteLine(s.Name);
+
             //string document = parser.GetFullHtmlContent(htmlNode);
             //Console.WriteLine(document);
 
